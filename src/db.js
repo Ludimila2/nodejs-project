9d71cb3ef2d786797232adf7e187769b760785d8
Ludimila Oliveira <ludimillaoliveira9@gmail.com>
2020-12-02 10:02:23 -0300

    pseudo banco de dados

---
+++ src/db.js
@@ -0,0 +1,26 @@
+const sequence = {
+    _id: 1,
+    get id() { return this._id++ }
+}
+
+const produtos = {}
+
+// se o produto já estiver setado, vai substituir pela versão mais nova, se não; vai adicionar um novo
+function salvarProduto(produto) {
+    if (!produtos.id) produto.id = sequence.id 
+    produtos[produto.id] = produto
+    return produto
+}
+
+// retornando um produto pelo id
+function getProduto(id) {
+    return produtos[id] || {} 
+}
+
+// retornando todos os produtos
+function getProdutos() {
+    return Object.values(produtos)
+}
+
+// retornando as funções para fora do arquivo de modules
+module.exports = { salvarProduto, getProduto, getProdutos }
