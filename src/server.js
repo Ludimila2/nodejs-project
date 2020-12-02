84c92d37b4ed2af5116bbcd016ba08f012f5e824
Ludimila Oliveira <ludimillaoliveira9@gmail.com>
2020-12-02 10:02:00 -0300

    importando dependencias e retornando produtos do banco de dados

---
+++ src/server.js
@@ -0,0 +1,31 @@
+const porta = 3003
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const bancoDeDados = require('./db')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// retorna todos os produtos
+app.get('/produtos', (req, res, next) => {
+    res.send(bancoDeDados.getProdutos())
+})
+
+// retornando um produto específico pelo id
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+// salvando um produto
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto)
+})
+
+app.listen(porta, () => {
+    console.log(`Servidor está executando na porta ${porta}.`)
+})
